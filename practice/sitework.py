64f43f61eb996f6094243fddbe4f4ea2fbdf863c
MmArKIzZz <113737670+MmArKIzZz@users.noreply.github.com>
2022-12-08 18:47:18 +0300

    Update sitework.py

--- practice/sitework.py
+++ practice/sitework.py
@@ -1,26 +1,13 @@
-import requests
-import bs4
-body=[]
-info=[]
-final=[]
-page_number=1
-while page_number<=5:
-    page=requests.get('https://msk.spravker.ru/avtoservisy-avtotehcentry/?page='+str(page_number))
-    r_page=bs4.BeautifulSoup(page.content, 'html.parser')
-    widgets=r_page.find_all("div",class_="org-widget__in")
-    for widget in widgets:
-        body.append(widget)
-    for i in range(0,len(body)):
-        d=[]
-        name= body[i].find('h3')
-        com=body[i].find('p')
-        adress = body[i].find('span', class_="org-widget-header__meta org-widget-header__meta--location")
-        d.append(name.text.strip())
-        d.append(adress.text.strip())
-        phone = body[i].find_all('dd')
-        for phones in phone:
-            d.append(phones.text.strip())
-        final.append(d)
-    page_number+=1
-print(final)
+import re
+import ssl
+import urllib.request
+
+ssl._create_default_https_context = ssl._create_unverified_context
+page=1
+while page<=5:
+    site = urllib.request.urlopen(f"https://msk.spravker.ru/avtoservisy-avtotehcentry/?page={page}").read().decode()
+    patern='(?:[\w\W]*?org-widget-header__title-link">)([^<]+)(?:[\w\W]*?header__meta--location">)\s*([А-я,\- .0-9]*)(?:[\w\W]*?="spec__value">)\s*([^<]*)(?:[\w\W]*?="spec__value">)([\w\W][^<]+)'
+    match=re.findall(patern,site)
+    print(match)
+    page+=1
 
